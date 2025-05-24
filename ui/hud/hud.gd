6297b661a81952fb02a26330250ceb2b45277d41
Yoshi Debat <yoshiwebdev@gmail.com>
2025-05-24 14:52:45 -0300

    mejorar estilos del hud

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -4,12 +4,14 @@ extends CanvasLayer
 
 @onready var moneda_label: Label = $MonedasPanel/HBoxContainer/MonedaLabel
 
-func actualizar_label_monedas(monedas_recolectadas: int, monedas_totales: int):
-	moneda_label.text = str(monedas_recolectadas).pad_zeros(2) + "/" + str(monedas_totales).pad_zeros(2)
+func actualizar_label_monedas(monedas_recolectadas: int, _monedas_totales: int):
+	moneda_label.text = str(monedas_recolectadas).pad_zeros(2)
 
 func actualizar_vidas(vidas_restantes: int):
 	for nro_corazon in corazones.size():
 		if nro_corazon < vidas_restantes:
-			corazones[nro_corazon].show()
+			#corazones[nro_corazon].show()
+			corazones[nro_corazon].modulate = Color(1, 1, 1, 1)
 		else:
-			corazones[nro_corazon].hide()
+			#corazones[nro_corazon].hide()
+			corazones[nro_corazon].modulate = Color(1, 1, 1, 0)
