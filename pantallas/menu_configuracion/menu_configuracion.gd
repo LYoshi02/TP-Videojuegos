7f6913217b3e23a39fe049e1bfb5a2ea13243e94
Yoshi Debat <yoshiwebdev@gmail.com>
2025-06-16 13:44:48 -0300

    agregar ajuste de pantalla completa en menu de configuracion general

--- pantallas/menu_configuracion/menu_configuracion.gd
+++ pantallas/menu_configuracion/menu_configuracion.gd
@@ -1,7 +1,20 @@
 extends Control
 
+@onready var check_box_full_screen: CheckBox = $Panel/Panel/ContenedorBotones/HBoxContainer/CheckBoxFullScreen
+
+func _ready() -> void:
+	var current_window_mode = DisplayServer.window_get_mode()
+	if current_window_mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		check_box_full_screen.button_pressed = true
+
 func _on_borrar_datos_boton_pressed() -> void:
 	GLOBAL.borrar_progreso()
 
 func _on_volver_boton_pressed() -> void:
 	get_tree().change_scene_to_file(GLOBAL.PANTALLAS["MENU_PRINCIPAL"])
+
+func _on_check_box_full_screen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
