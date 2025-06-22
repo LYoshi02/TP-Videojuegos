3dabd85738580c56ce7f1488a9f8ffed7f79bdef
Yoshi Debat <yoshiwebdev@gmail.com>
2025-06-22 00:34:31 -0300

    agregar carpincho

---
+++ juego/animales/carpincho/carpincho.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_activacion: RayCast2D = $RayCastActivacion
+@onready var ray_cast_derecha: RayCast2D = $RayCastDerecha
+@onready var ray_cast_izquierda: RayCast2D = $RayCastIzquierda
+
+
+var velocidad = 40
+var direccion = 1
+var activo = false
+var caminando = false
+
+func _process(delta: float) -> void:
+	if not activo:
+		if not ray_cast_activacion.is_colliding():
+			activo = true
+			activar_parado()
+	elif caminando:
+		if ray_cast_derecha.is_colliding():
+			direccion = -1
+			animated_sprite_2d.flip_h = true
+		if ray_cast_izquierda.is_colliding():
+			direccion = 1
+			animated_sprite_2d.flip_h = false
+		
+		position.x += direccion * velocidad * delta
+
+func activar_parado():
+	animated_sprite_2d.play("parado")
+	
+func activar_caminar():
+	animated_sprite_2d.play("caminando")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "parado":
+		activar_caminar()
+		caminando = true
